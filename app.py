bfaf552eff91f05fdc45ad330e85bcb2b8f43e0b
Chandragiri Suhel <chandragirisuhel123@gmail.com>
2026-04-18 20:17:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,7 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
-    # Import modules (NO *)
     import auth
     import chat
-
-    # If using Blueprints (better structure)
-    # app.register_blueprint(auth.bp)
-    # app.register_blueprint(chat.bp)
 
     return app
